508698262472a9906e6566a60277a30b2c4c8cad
tushal108 <147639515+tushal108@users.noreply.github.com>
2023-10-11 21:35:48 +0500

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,13 +9,13 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', ''))
-    API_HASH = str(getenv('API_HASH', ''))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    API_ID = int(getenv('API_ID', '29049376'))
+    API_HASH = str(getenv('API_HASH', '271bb7e6d64290dfd57a21204a525b9b'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6301678487:AAFhSv3hx4tz1BrsUiDjSm7Sx2k9HOGB8MQ'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001910086798'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
